b3e04498d0a6c7fc5a772a62c886905e0e089951
jjburitik <jjburitik@gmail.com>
2020-06-11 03:28:01 +0000

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Prints a specified string
+ *Return: 0 if the programming ran to completion
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
