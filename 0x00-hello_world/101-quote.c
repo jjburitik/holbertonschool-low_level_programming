d8b390a8c60bb2d960a923d13d06fc812b542a00
jjburitik <jjburitik@gmail.com>
2020-06-12 22:27:21 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -
+ * main -  UNIX is basically a simple operating system,
+ * but you have to be a genius to understand the simplicity
  *
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
