3f4e1c6bcc96e99cf1e4537bafb4cfa0ab4aebaa
jjburitik <jjburitik@gmail.com>
2020-06-15 23:39:06 +0000

    add 3 & update holberton.h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *_islower - function that checks for lowercase character.
+ *@c: character to check
+ *Return: 1 for lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
