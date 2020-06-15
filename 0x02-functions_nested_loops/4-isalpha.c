e0e38f4286796b2da248488da138e64f5b39cdf2
jjburitik <jjburitik@gmail.com>
2020-06-15 23:52:12 +0000

    add 4 & update holberton.h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *_islower - function that checks for alphabetic character
+ *@c: character to check
+ *Return: 1 lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return (1);
+  else
+    return (0);
+}
