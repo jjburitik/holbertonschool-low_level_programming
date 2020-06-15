e2e2f52eada8984e0a1735c9489de375d7b1a5ee
jjburitik <jjburitik@gmail.com>
2020-06-15 22:42:31 +0000

    add 2 & update holberton.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int C;
+for (C = 0; C <= 10; C++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+}
