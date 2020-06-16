5afb2722b9c675a59b2ecb441d7b6be9dacfbc56
jjburitik <jjburitik@gmail.com>
2020-06-16 17:15:45 +0000

    add 8 & update holberton.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - function that prints every minute of the day of Jack Bauer,
+ *starting from 00:00 to 23:59.
+ *
+ *Return: void
+ */
+void jack_bauer(void)
+{
+int hour, min;
+for (hour = 0; hour < 23; hour++)
+{
+for (min = 0; min < 60; min++)
+{
+_putchar('0' + hour / 10);
+_putchar('0' + hour % 10);
+_putchar(':');
+_putchar('0' + min / 10);
+_putchar('0' + min % 10);
+_putchar('\n');
+}
+}
+}
