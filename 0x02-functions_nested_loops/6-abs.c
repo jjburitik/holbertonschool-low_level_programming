94e0beae8f46c9d809d6443e855e744d57abc6b6
jjburitik <jjburitik@gmail.com>
2020-06-16 02:00:41 +0000

    add 6 & update holberton.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *print_sign - function that computes the absolute value of an integer.
+ *@n: number to check
+ *Return: the absolute value of n
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
