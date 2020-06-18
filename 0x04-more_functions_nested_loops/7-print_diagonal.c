ac766b7b2d3f06b95f4486a9d7474f84b458964f
jjburitik <jjburitik@gmail.com>
2020-06-18 21:26:40 +0000

    Add 7 & update holberton

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_diagonal - function that draws a diagonal line on the terminal.
+ *@n: is the number of times the character \ should be printed
+ *
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+int control, spaces;
+for (control = 0 ; control < n; control ++)
+{
+for (spaces = 0 ; spaces < control; spaces++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');      
+}
+if (n <= 0)
+_putchar('\n');
+}
