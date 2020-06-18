b8ee329b2047b207faf4ba6d45b38fa7f1778353
jjburitik <jjburitik@gmail.com>
2020-06-18 20:37:29 +0000

    Add 5 & update holberton

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- *more_numbers - function that prints 10 times the numbers, 
+ *more_numbers - function that prints 10 times the numbers,
  *from 0 to 14, followed by a new line.
  *
  *Return: void
