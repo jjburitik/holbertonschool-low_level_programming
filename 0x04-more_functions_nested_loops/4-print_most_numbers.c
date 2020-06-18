d441e07db0ac895e741c239d703590ff8c547f29
jjburitik <jjburitik@gmail.com>
2020-06-18 19:07:20 +0000

    Add 4, _putchar & update holberton

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers - function that prints the numbers, from 0 to 9,
+ *followed by a new line.
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+int a;
+for (a = 0 ; a < 10 ; a++)
+{
+if (a != 2 && a != 4)
+_putchar('0' + a);
+}
+_putchar('\n');
+}
