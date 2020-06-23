f52066d1cbb5279145c322a68d47cba5a8e37615
jjburitik <jjburitik@gmail.com>
2020-06-23 14:50:42 +0000

    update 1 $ update holberton

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *reset_to_98 - function that takes a pointer to an int as 
+ *reset_to_98 - function that takes a pointer to an int as
  *parameter and updates the value it
  *@n: A pointer to an int that to update
  *
@@ -8,5 +8,7 @@
  */
 void reset_to_98(int *n)
 {
+
 *n = 98;
+
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,7 @@
+/*Holberton header files*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -6,3 +10,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+#endif
