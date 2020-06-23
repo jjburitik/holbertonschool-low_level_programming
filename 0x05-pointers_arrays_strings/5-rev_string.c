61bb31283202cb739a020844e8ae324ca99ae646
jjburitik <jjburitik@gmail.com>
2020-06-23 01:00:53 +0000

    add 5 & update holberton

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *rev_string - function that reverses a string
+ *@s: String to reverse
+ *
+ *Return: void
+ */
+void rev_string(char *s)
+{
+int a, b, temp;
+a = 0;
+while (s[a + 1] != '\0')
+a++;
+for (b = 0; b <= a / 2; b++)
+{
+temp = s[b];
+s[b] = s[a - b];
+s[a -b] = temp;
+}
+}
