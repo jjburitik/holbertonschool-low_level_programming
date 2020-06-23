92f7b43f7382b65d4d96dd1c2e5061ec22f26783
jjburitik <jjburitik@gmail.com>
2020-06-23 14:44:43 +0000

    prueba holberton

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,7 +1,3 @@
-/*Holberton header files*/
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,4 +6,3 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-#endif
