fb057452854fa733675d706acba792ec4e9d01c4
jjburitik <jjburitik@gmail.com>
2020-06-24 01:37:55 +0000

    add 9 & update holberton

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strcpy - function that copies the string pointed to by src,
+ *including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int a;
+
+for (a = 0; src[a] != 0; a++)
+dest[a] = src[a];
+dest[a] = 0;
+return (dest);
+}
